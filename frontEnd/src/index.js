2b1cf96668552082867675c4cd5bc9ac051f8194
Tatyana Shaferova <191tany@gmail.com>
2023-03-19 01:44:45 +0200

    add theme

--- frontEnd/src/index.js
+++ frontEnd/src/index.js
@@ -3,19 +3,41 @@ import ReactDOM from "react-dom/client";
 
 import "./styles/style.scss";
 
-import App from "./App";
-
 //import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
 import AppRouter from "Routes/AppRouter";
+import { ThemeProvider, createTheme } from "@mui/material";
+import { blue, pink } from "@mui/material/colors";
+
+const theme = createTheme({
+  palette: {
+    primary: blue,
+    secondary: pink,
+    // primary: {
+    //   light: "#757ce8",
+    //   main: "#3f50b5",
+    //   dark: "#002884",
+    //   contrastText: "#fff",
+    // },
+    // secondary: {
+    //   light: "#ff7961",
+    //   main: "#f44336",
+    //   dark: "#ba000d",
+    //   contrastText: "#000",
+    // },
+  },
+});
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   //<React.StrictMode>
-  <BrowserRouter>
-    <AppRouter />
-  </BrowserRouter>
+  <ThemeProvider theme={theme}>
+    <BrowserRouter>
+      <AppRouter />
+    </BrowserRouter>
+  </ThemeProvider>
+
   //</React.StrictMode>
 );
 
